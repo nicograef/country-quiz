cd184b4507ecada9dc50e12c07c7c09965d97b94
Nico Gräf <graef.nico@gmail.com>
2019-10-02 20:06:13 +0200

    added coverage support

--- src/index.test.js
+++ src/index.test.js
@@ -7,14 +7,17 @@ describe('newQuestion', () => {
     expect(question).toHaveProperty('answer')
     expect(question).toHaveProperty('options')
   })
+
   it('should have 4 options by default', () => {
     const question = newQuestion()
     expect(question.options.length).toBe(4)
   })
+
   it('should accept a numberOfOptions argument', () => {
     const question = newQuestion(undefined, 2)
     expect(question.options.length).toBe(2)
   })
+
   it('should accept a quizMode argument ', () => {
     const question = newQuestion('capital-to-country')
     expect(question.question).not.toMatch(/.svg/)
@@ -30,6 +33,7 @@ describe('newRandomQuestion', () => {
     expect(question).toHaveProperty('options')
     expect(question).toHaveProperty('quizMode')
   })
+
   it('should accept a numberOfOptions argument', () => {
     const question = newRandomQuestion(2)
     expect(question.options.length).toBe(2)
@@ -41,20 +45,32 @@ describe('newQuiz', () => {
     const quiz = newQuiz()
     expect(quiz).toHaveProperty('quizMode', 'flag-to-country')
   })
+
   it('should have 5 questions by default', () => {
     const quiz = newQuiz()
     expect(quiz.questions.length).toBe(5)
   })
+
   it('should accept a quizMode argument', () => {
     const quiz = newQuiz('flag-to-capital')
     expect(quiz).toHaveProperty('quizMode', 'flag-to-capital')
   })
+
   it('should accept a numberOfQuestions argument', () => {
     const quiz = newQuiz(undefined, 8)
     expect(quiz.questions.length).toBe(8)
   })
+
   it('should accept a numberOfOptions argument', () => {
     const quiz = newQuiz(undefined, undefined, 2)
     expect(quiz.questions[0].options.length).toBe(2)
+  })
+
+  it('should use random questions in mixed mode', () => {
+    const quiz = newQuiz('mixed')
+    expect(quiz).toHaveProperty('quizMode', 'mixed')
+    quiz.questions.forEach(question => {
+      expect(question).toHaveProperty('quizMode')
+    })
   })
 })
